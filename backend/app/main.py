85ffdc325c4e62a31023f6f4906ab017878d4f33
alibro005 <itsalibro9@gmail.com>
2026-08-19 18:53:58 +0500

    fix: add additional allowed origin for CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://qumly.vercel.app"
+        "https://qumly.vercel.app",
+        "https://qumly.me"
     ],
     allow_credentials=True,
     allow_methods=["*"],
